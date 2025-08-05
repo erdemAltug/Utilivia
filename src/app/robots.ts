230a592559536583e3b310e39eea441f72d7fd16
altug.erdem <altug.erdem@enuygun.com>
2025-08-05 22:59:46 +0300

    🔧 Fix robots.txt sitemap URL

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://utilivia.com/sitemap.xml',
+    sitemap: 'https://www.utilivia.com/sitemap.xml',
   }
 } 
